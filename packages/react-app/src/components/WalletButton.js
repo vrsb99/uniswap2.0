4270e879b66b743a4ecefac0a576f11d64cc9de6
vrsb99 <vigneshrsb99@gmail.com>
2022-09-13 22:43:10 -0500

    WalletButton

--- packages/react-app/src/components/WalletButton.js
+++ packages/react-app/src/components/WalletButton.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { shortenAddress, useEthers, useLookupAddress } from "@usedapp/core";
+
+import styles from "../styles";
 
 const WalletButton = () => {
-  return (
-    <div>WalletButton</div>
-  )
-}
+  const [accountAddress, setAccountAddress] = useState("");
 
-export default WalletButton
+  const { ens } = useLookupAddress();
+  const { account, activateBrowserWallet, deactivate } = useEthers();
+
+  useEffect(() => {
+    if (ens) {
+      setAccountAddress(ens);
+    } else if (account) {
+      setAccountAddress(shortenAddress(account));
+    } else {
+      setAccountAddress("");
+    }
+  }, [account, ens, setAccountAddress]);
+
+  return (
+    <button
+      onClick={() => {
+        if (!account) {
+          activateBrowserWallet();
+        } else {
+          deactivate();
+        }
+      }}
+      className={styles.walletButton}
+    >
+      {accountAddress || "Connect Wallet"}
+    </button>
+  );
+};
+
+export default WalletButton;
