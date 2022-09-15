8ed4767fd5233af142394b7514f2cdc93c351909
vrsb99 <vigneshrsb99@gmail.com>
2022-09-15 18:38:57 -0500

    Pools

--- packages/react-app/src/config.js
+++ packages/react-app/src/config.js
@@ -1,10 +1,11 @@
 import { Goerli } from "@usedapp/core";
 
-export const ROUTER_ADDRESS = "[YOUR ADDRESS HERE]"; 
+export const ROUTER_ADDRESS = 
+"0xe75c548dfba9155746f7cc241910aa76d490c5cc"; 
 
 export const DAPP_CONFIG = {
   readOnlyChainId: Goerli.chainId,
   readOnlyUrls: {
-    [Goerli.chainId]: "[YOUR URL HERE]",
+    [Goerli.chainId]: "https://eth-goerli.g.alchemy.com/v2/VFVeT3J9yUX9SZs4FStpZUZSJyIUolda",
   },
 };
